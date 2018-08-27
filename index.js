0e5a79577825870cd6762c7dd8023cc73911c8f7
Stefan Peters <Stefan.Peters@gbv.de>
2018-08-27 08:13:50 +0200

    Include JSKOS spec version number
    Closes #2.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const validate = require("./lib/validate")
 const identifiers = require("./lib/identifiers")
 const tools = require("./lib/tools")
+const version = "0.4.2"
 
-module.exports = Object.assign({ validate }, identifiers, tools)
+module.exports = Object.assign({ validate, version }, identifiers, tools)
