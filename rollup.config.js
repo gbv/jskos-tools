fe8d67c7568df7debaa684261ab66a3381bbd770
Stefan Peters <Stefan.Peters@gbv.de>
2020-08-20 14:38:21 +0200

    Update rollup configuration with Babel support and minification (#19)

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,37 @@
 import pkg from "./package.json"
 import commonjs from "@rollup/plugin-commonjs"
 import { nodeResolve } from "@rollup/plugin-node-resolve"
+import babel from "@rollup/plugin-babel"
+import terser from "@yuloh/rollup-plugin-terser"
 
 export default [
+  // browser-friendly UMD build
   {
     input: "index.js",
+    output: {
+      name: "jskos",
+      file: pkg.browser,
+      format: "umd",
+    },
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      babel(),
+      terser(),
+    ],
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: "index.js",
+    external: ["lodash"],
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" },
+    ],
     plugins: [
       commonjs(),
-      nodeResolve({browser: true}),
-    ],
-    output: [
-      { file: pkg.module, format: "es" },
+      terser(),
     ],
   },
 ]
