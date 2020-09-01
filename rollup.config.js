3e7e534f1c5ab32a82aaf0db9f7b2b65d85e3ff7
Stefan Peters <Stefan.Peters@gbv.de>
2020-09-01 12:03:36 +0200

    Update Rollup and release configuration

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs"
 import resolve from "@rollup/plugin-node-resolve"
 import babel from "@rollup/plugin-babel"
 import json from "@rollup/plugin-json"
+import license from "rollup-plugin-license"
 
 export default [
   {
@@ -16,6 +17,18 @@ export default [
       resolve(),
       commonjs(),
       json(),
+      // Add a plugin that assembles licenses for dependencies
+      license({
+        banner: {
+          commentStyle: "ignored",
+          content: `${pkg.name} v${pkg.version}\nCopyright (c) 2018 Verbundzentrale des GBV (VZG)\n@license ${pkg.license}\n\nFor dependency license information, please see ${pkg.browser}.LICENSES.txt.`,
+        },
+        thirdParty: {
+          output: {
+            file: pkg.browser + ".LICENSES.txt",
+          },
+        },
+      }),
       babel({
         babelHelpers: "bundled",
       }),
